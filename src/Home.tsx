d08cb60d061ebf1dc6782ab1f5c6279f20cdea1e
“Some <ds4nch3zz@gmail.com>
2022-06-13 23:13:38 +0300

    cut supply

--- src/Home.tsx
+++ src/Home.tsx
@@ -672,7 +672,7 @@ const Home = (props: HomeProps) => {
                                 renderer={renderEndDateCounter}
                               />}
                             {wallet && isActive &&
-                              <h3>TOTAL MINTED : {itemsRedeemed} / {itemsAvailable}</h3>}
+                              <h3>TOTAL MINTED : {itemsRedeemed} / 963</h3>}
                             {wallet && isActive && <BorderLinearProgress variant="determinate"
                                                                          value={100 - (itemsRemaining * 100 / itemsAvailable)}/>}
                             <br/>
